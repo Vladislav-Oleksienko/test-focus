8e7634d9e53964d2150f74ba3ea69442d0ace357
Vladislav-Oleksienko <splinfao1@gmail.com>
2023-04-16 19:09:19 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+console.log("Запрос данных...");
+
+const req = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Подготовка данных...");
+
+    const product = {
+      name: "TV",
+      price: 2000,
+    };
+
+    resolve(product);
+  }, 2000);
+});
+
+req.then((product) => {
+  const req2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      product.status = "order";
+      resolve(product);
+    }, 2000);
+  });
+
+  req2.then((data) => {
+    console.log(data);
+  });
+});
